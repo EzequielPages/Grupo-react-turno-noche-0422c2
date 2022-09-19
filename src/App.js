85577045b70f168327dbff0ed9129504660c180d
Nacho35 <ignacioevil@gmail.com>
2022-09-19 17:58:34 -0300

    footer sin estilos mas config de taiwings paleta de colores variables

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import Banner from '../src/Components/Banner.js'
-import NavBar from '../src/Components/NavBar.js'
-import Home from './Components/Home.js'
-import './App.css'
-
+import React from "react";
+// import Banner from "../src/Components/Banner.js";
+// import NavBar from "../src/Components/NavBar.js";
+// import Home from "./Components/Home.js";
+import Footer from "./Components/Footer.js";
+import "./App.css";
 
 export default function App() {
   return (
-   <>
-    <Banner />
-    <NavBar />
-    <Home />
-   </>
-  )
+    <>
+      <Footer />
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      "first-color": "#22353c",
+      "second-color": "#dea26c",
+      "third-color": "#6f4837",
+      "fourth-color": "#05080d",
+      "fifth-color": "#a26e49",
+      "sixth-color": "#3d272a",
+      "seventh-color": "#e7e3e2",
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
-}
+};
