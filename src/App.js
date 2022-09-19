fa718c872525e6e4e8033f02ad53d0b0df284745
Nicolás Valiconto <nvaliconto@outlook.com>
2022-09-19 20:24:13 -0300

    Boton y programacion implementado, falta maquetar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react'
 import Banner from '../src/Components/Banner.js'
 import NavBar from '../src/Components/NavBar.js'
 import Home from './Components/Home.js'
+import { ThemeProvider } from './Components/ThemeContext.js'
 import './App.css'
 
 
 export default function App() {
   return (
-   <>
-    <Banner />
-    <NavBar />
-    <Home />
-   </>
+    <ThemeProvider>
+      <Banner />
+      <NavBar />
+      <Home />
+    </ThemeProvider>
   )
 }
