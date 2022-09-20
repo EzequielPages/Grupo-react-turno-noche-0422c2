bd06fead751c0b106f1542f27bc7a2daec13cf68
Nicolás Valiconto <nvaliconto@outlook.com>
2022-09-20 12:35:21 -0300

    Dark mode implementado, faltan ajustes

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faStarHalf } from "@fortawesome/free-regular-svg-icons"
 import { faStar } from "@fortawesome/free-solid-svg-icons"
 import "./Card.css"
@@ -7,38 +7,38 @@ import "./Card.css"
 
 
 
-const Card = ({image, country, plan, reviews, duration, price,}) => {
+const Card = ({ image, country, plan, reviews, duration, price, }) => {
   return (
 
-    <div className="border shadow-sm bg-white ring-2 flex flex-col justify-start card-size" >
-        <img className="overflow-hidden size-image" src={image} alt={image}/> 
-        <div>
+    <div className="border shadow-sm bg-white dark:bg-black dark:text-white ring-2 flex flex-col justify-start card-size" >
+      <img className="overflow-hidden size-image" src={image} alt={image} />
+      <div>
 
         <p className="m-1 text-gray-400">{country}</p>
         <h5 className="m-1 text-black">{plan}</h5>
         <div className="m-1 flex gap-6">
-            <div>
-            <FontAwesomeIcon icon={faStar} className="text-yellow-400"/>
-            <FontAwesomeIcon icon={faStar} className="text-yellow-400"/>
-            <FontAwesomeIcon icon={faStar} className="text-yellow-400"/>
-            <FontAwesomeIcon icon={faStar} className="text-yellow-400"/>
-            <FontAwesomeIcon icon={faStarHalf} className="text-yellow-400"/>
-            </div>
-            <p>{reviews} REVIEWS</p>
+          <div>
+            <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
+            <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
+            <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
+            <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
+            <FontAwesomeIcon icon={faStarHalf} className="text-yellow-400" />
+          </div>
+          <p>{reviews} REVIEWS</p>
         </div>
         <div className="flex justify-between border-t-2 info-style">
-        <span className="m-1 text-gray-400">{duration}</span><span className="text-green-500">USD${price}</span>
+          <span className="m-1 text-gray-400">{duration}</span><span className="text-green-500">USD${price}</span>
         </div>
 
-        </div>  
-        <button className="hover:btn-primary transition-all duration-1000 border btn-style h-20" >MÁS INFORMACIÓN
-        </button>
+      </div>
+      <button className="hover:btn-primary transition-all duration-1000 border btn-style h-20" >MÁS INFORMACIÓN
+      </button>
 
-         
+
 
     </div>
 
-    
+
   )
 }
 
