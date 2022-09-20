c11320ca5b0a61429e6c2e3b5e7d4225e83d8640
Nacho35 <ignacioevil@gmail.com>
2022-09-19 21:40:08 -0300

    footer sin logo y sin iconos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-// import Banner from "../src/Components/Banner.js";
-// import NavBar from "../src/Components/NavBar.js";
-// import Home from "./Components/Home.js";
+import Banner from "../src/Components/Banner.js";
+import NavBar from "../src/Components/NavBar.js";
+import Home from "./Components/Home.js";
 import Footer from "./Components/Footer.js";
 import "./App.css";
 
@@ -9,6 +9,9 @@ export default function App() {
   return (
     <>
       <Footer />
+      <Banner />
+      <NavBar />
+      <Home />
     </>
   );
 }
--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Banner = () => {
   return (
-      <div className='w-full bg-cyan-100 font-bold underline-offset-1 p-1'>
-        <div className='flex space-x-3 place-content-center'>
-             <p>Obten el 10% en tu próximo Viaje con este cupon</p>
-             <a className='underline' href='link'>Clik Here</a>
-        </div>        
+    <div className="w-full bg-cyan-100 font-bold underline-offset-1 p-1">
+      <div className="flex space-x-3 place-content-center">
+        <p>Obten el 10% en tu próximo Viaje con este cupon</p>
+        <a className="underline" href="link">
+          Click Here!
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
