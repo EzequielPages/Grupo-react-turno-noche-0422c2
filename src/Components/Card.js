2064926bd00626cd8a7aebb6eed33b0b3049b810
Ezequiel Pages <ezepages333111@gmail.com>
2022-09-21 15:02:12 -0300

    Cards en Home (Terminado el responsive y animaciones)

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,21 +2,28 @@ import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import { faStarHalf } from "@fortawesome/free-regular-svg-icons"
 import { faStar } from "@fortawesome/free-solid-svg-icons"
+import { faLocationDot} from "@fortawesome/free-solid-svg-icons"
 import "./Card.css"
 
 
 
 
-const Card = ({image, country, plan, reviews, duration, price,}) => {
+const Card = ({image, country, plan, reviews, duration, price, textDeploy,}) => {
   return (
 
-    <div className="border shadow-sm bg-white ring-2 flex flex-col justify-start card-size" >
-        <img className="overflow-hidden size-image" src={image} alt={image}/> 
+    <div className="border border-gray-600 shadow-sm bg-white flex flex-col justify-start card-size  md:bg-white-500 " >
+      <div className="containerAnimated">
+        <img className="overflow-hidden size-image imageAnimated" src={image} alt={image}/> 
+          <div class="overlayAnimated">
+              <div class="textAnimated italic text-sm"><FontAwesomeIcon icon={faLocationDot}/> {textDeploy}
+              </div>
+          </div>
+      </div>
         <div>
 
-        <p className="m-1 text-gray-400">{country}</p>
-        <h5 className="m-1 text-black">{plan}</h5>
-        <div className="m-1 flex gap-6">
+        <p className="m-2 text-gray-400">{country}</p>
+        <h6 className="m-2 font-medium italic text-black">{plan}</h6>
+        <div className="m-2 flex gap-6">
             <div>
             <FontAwesomeIcon icon={faStar} className="text-yellow-400"/>
             <FontAwesomeIcon icon={faStar} className="text-yellow-400"/>
@@ -27,11 +34,11 @@ const Card = ({image, country, plan, reviews, duration, price,}) => {
             <p>{reviews} REVIEWS</p>
         </div>
         <div className="flex justify-between border-t-2 info-style">
-        <span className="m-1 text-gray-400">{duration}</span><span className="text-green-500">USD${price}</span>
+        <span className="m-1 text-gray-400">{duration}</span><span className="text-green-600">USD${price}</span>
         </div>
 
         </div>  
-        <button className="hover:btn-primary transition-all duration-1000 border btn-style h-20" >MÁS INFORMACIÓN
+        <button className="hover:btn-primary transition-all duration-1000 border btn-style h-20 italic" >MÁS INFORMACIÓN
         </button>
 
          
--- src/Components/CardContainer.js
+++ src/Components/CardContainer.js
@@ -1,24 +1,32 @@
 import React from "react";
 import Card from "./Card";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faPlaneDeparture } from "@fortawesome/free-solid-svg-icons"
+import { faLocationDot} from "@fortawesome/free-solid-svg-icons"
 
 const CardContainer = () => {
   return (
-    <section className="flex flex-col justify-center items-center">
-        <Card 
+    <div className="bg-base-100">
+      <h5 className="text-center italic text-blue-500">Recorré el mundo <FontAwesomeIcon icon={faPlaneDeparture}/></h5>
+      <h3 className="text-center font-medium textarea-ghost"><FontAwesomeIcon icon={faLocationDot}/> Tours más famosos</h3>
+    <section className="container mx-auto md:grid-flow-col-dense md:items-center sm:flex-row justify-center md:justify-evenly md:gap-2 sm:gap-1 items-center grid md:grid-cols-2 md:grid-rows-2 lg:grid-cols-4 lg:grid-rows-1">
+        <Card
         image = "/Media/Qatar.jpg"
         country = "QATAR"
         plan = "FIFA WORLD CUP 2022"
         duration = "21 DÍAS"
         reviews= "31"
         price= "5500"
+        textDeploy="DOHA"
         />
-        <Card 
+        <Card
         image = "/Media/Sudafrica.jpg"
         country = "SUDÁFRICA"
         plan = "CONOCÉ LA SABANA"
         duration = "9 DÍAS"
         reviews= "92"
         price= "1900"
+        textDeploy="SABI SABI"
         />
         <Card 
         image = "/Media/Cataratas.jpg"
@@ -27,6 +35,7 @@ const CardContainer = () => {
         duration = "7 DÍAS"
         reviews= "129"
         price= "790"
+        textDeploy="MISIONES"
         />
         <Card 
         image = "/Media/España.jpg"
@@ -35,8 +44,10 @@ const CardContainer = () => {
         duration = "10 DÍAS"
         reviews= "101"
         price= "3900"
+        textDeploy="SAGRADA FAMILIA"
         />
     </section>
+     </div>
   )
 }
 
