815f22a6628328279fbc989c6d11c7348379e52f
Nacho35 <ignacioevil@gmail.com>
2022-09-20 22:01:24 -0300

    todo en orden

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Banner from '../src/Components/Banner.js'
-import NavBar from '../src/Components/NavBar.js'
-import Home from './Components/Home.js'
-import CardContainer from './Components/CardContainer.js'
-import { ThemeProvider } from './Components/ThemeContext.js'
-import './App.css'
-
+import React from "react";
+import Banner from "../src/Components/Banner.js";
+import NavBar from "../src/Components/NavBar.js";
+import Home from "./Components/Home.js";
+import Footer from "./Components/Footer";
+import CardContainer from "./Components/CardContainer.js";
+import { ThemeProvider } from "./Components/ThemeContext.js";
+import "./App.css";
 
 export default function App() {
   return (
@@ -15,7 +15,8 @@ export default function App() {
         <NavBar />
         <Home />
         <CardContainer />
+        <Footer />
       </ThemeProvider>
     </>
-  )
+  );
 }
