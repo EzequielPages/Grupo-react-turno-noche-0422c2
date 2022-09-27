c3a2f15d5181c5785644f17183a8bf082f18252b
Nicolás Valiconto <nvaliconto@outlook.com>
2022-09-27 18:21:17 -0300

    cambios

--- src/Components/Share.js
+++ src/Components/Share.js
@@ -1,14 +1,31 @@
 import React from 'react'
-import { FacebookShareButton, FacebookIcon } from "react-share";
+import {
+    FacebookShareButton,
+    FacebookIcon,
+    WhatsappShareButton,
+    WhatsappIcon,
+    LinkedInShareButton,
+    LinkedInIcon,
+} from "react-share";
 
 const Share = () => {
 
-    const shareUrl = 'http://google.com.ar/'
+    const shareUrl = 'http://www.google.com.ar/'
 
     return (
-        <FacebookShareButton url={shareUrl}>
-            <FacebookIcon size={40} />
-        </FacebookShareButton>
+        <>
+            <FacebookShareButton url={shareUrl}>
+                <FacebookIcon size={40} round={true} />
+            </FacebookShareButton>
+
+            <WhatsappShareButton url={shareUrl}>
+                <WhatsappIcon size={40} round={true} />
+            </WhatsappShareButton>
+
+            <LinkedInShareButton url={shareUrl}>
+                <LinkedInIcon size={40} round={true} />
+            </LinkedInShareButton>
+        </>
     )
 }
 
