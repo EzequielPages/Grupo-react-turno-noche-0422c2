6e9a0467f148c3a3238b997aff374edc3c568197
Nicolás Valiconto <nvaliconto@outlook.com>
2022-09-27 16:47:05 -0300

    Boton facebook implementado

--- src/Components/Share.js
+++ src/Components/Share.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { FacebookShareButton, FacebookIcon } from "react-share";
 
 const Share = () => {
+
+    const shareUrl = 'http://google.com.ar/'
+
     return (
-        <div>Share</div>
+        <FacebookShareButton url={shareUrl}>
+            <FacebookIcon size={40} />
+        </FacebookShareButton>
     )
 }
 
