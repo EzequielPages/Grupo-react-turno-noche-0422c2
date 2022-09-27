174077103c7f7ee85811f9236f80bcf1b2f0368f
Nicolás Valiconto <nvaliconto@outlook.com>
2022-09-27 12:11:35 -0300

    Probando funcionalidad en light/dark en la navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,7 +3,7 @@ import Toggle from '../Components/Toggle.js'
 
 const NavBar = () => {
   return (
-    <div className="navbar bg-white dark:bg-black dark:text-white">
+    <div className="navbar bg-first-color text-second-color dark:bg-first-night-color dark:text-second-night-color">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
