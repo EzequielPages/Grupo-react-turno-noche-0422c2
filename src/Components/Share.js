c8a60a323fff18991d3b0616fd17b3503c02f5ad
Nicolás Valiconto <nvaliconto@outlook.com>
2022-09-28 15:11:20 -0300

    fondo listo

--- src/Components/Share.js
+++ src/Components/Share.js
@@ -20,37 +20,42 @@ const Share = () => {
 
     const shareUrl = 'http://www.google.com.ar/'
 
+    // 
     return (
         <>
-            <div class="flex items-center justify-center space-x-14 h-44  bg-third-color dark:bg-sixth-color ">
-                <p >Compartir:</p>
-                <FacebookShareButton url={shareUrl}>
-                    <FacebookIcon size={40} round={true} />
-                </FacebookShareButton>
+            <div class='bg-third-color text-seventh-color dark:bg-sixth-color dark:text-seventh-night-color' style={{
+                backgroundImage: `url('https://i.imgur.com/Y76wfux.png')`
+            }}>
+                <div class='text-center'><p class='font-bold underline'>COMPARTIR</p></div>
+                <div class='flex items-center justify-center space-x-14 h-20 '>
+                    <FacebookShareButton url={shareUrl}>
+                        <FacebookIcon size={40} round={true} />
+                    </FacebookShareButton>
 
-                <TwitterShareButton url={shareUrl}>
-                    <TwitterIcon size={40} round={true} />
-                </TwitterShareButton>
+                    <TwitterShareButton url={shareUrl}>
+                        <TwitterIcon size={40} round={true} />
+                    </TwitterShareButton>
 
-                <WhatsappShareButton url={shareUrl}>
-                    <WhatsappIcon size={40} round={true} />
-                </WhatsappShareButton>
+                    <WhatsappShareButton url={shareUrl}>
+                        <WhatsappIcon size={40} round={true} />
+                    </WhatsappShareButton>
 
-                <FacebookMessengerShareButton url={shareUrl}>
-                    <FacebookMessengerIcon size={40} round={true} />
-                </FacebookMessengerShareButton>
+                    <FacebookMessengerShareButton url={shareUrl}>
+                        <FacebookMessengerIcon size={40} round={true} />
+                    </FacebookMessengerShareButton>
 
-                <TelegramShareButton url={shareUrl}>
-                    <TelegramIcon size={40} round={true} />
-                </TelegramShareButton>
+                    <TelegramShareButton url={shareUrl}>
+                        <TelegramIcon size={40} round={true} />
+                    </TelegramShareButton>
 
-                <RedditShareButton url={shareUrl}>
-                    <RedditIcon size={40} round={true} />
-                </RedditShareButton>
+                    <RedditShareButton url={shareUrl}>
+                        <RedditIcon size={40} round={true} />
+                    </RedditShareButton>
 
-                <EmailShareButton url={shareUrl}>
-                    <EmailIcon size={40} round={true} />
-                </EmailShareButton>
+                    <EmailShareButton url={shareUrl}>
+                        <EmailIcon size={40} round={true} />
+                    </EmailShareButton>
+                </div>
             </div>
         </>
     )
