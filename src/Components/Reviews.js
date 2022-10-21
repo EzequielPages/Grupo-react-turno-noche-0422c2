45c5cbcf61ff3137d52cb688848d4e19a36ec06d
Naiozz <107516117+Naiozz@users.noreply.github.com>
2022-10-21 20:14:34 -0300

    Sombras por corregir en Reviews

--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -11,7 +11,7 @@ const Reviews = () => {
     <div className="flex flex-wrap -m-4">
       <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
         <div className="h-full text-center">
-          <img alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100 shadow-lg" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp"></img>
+          <img alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100 shadow" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp"></img>
           <p className="leading-relaxed">Excelente servicio. El traslado muy seguro, además te pueden orientar en todo momento ante cualquier consulta.</p>
           <ul className="flex justify-center pt-10">
                 <FontAwesomeIcon icon={faStar} className="text-second-color"/>
