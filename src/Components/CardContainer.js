63ca417537bf60ae9657f338ffddf0fe7e3ec1ce
Naiozz <107516117+Naiozz@users.noreply.github.com>
2022-10-25 19:44:30 -0300

    Padding de cards corregido.

--- src/Components/CardContainer.js
+++ src/Components/CardContainer.js
@@ -29,7 +29,7 @@ const CardContainer = () => {
 
   return (
     <div className="bg-base-200 dark:bg-fourth-color px-4 pt-4 pb-20">
-      <h5 className="text-center text-4xl font-bold text-first-color dark:text-seventh-color pb-2">
+      <h5 className="text-center text-4xl font-bold text-first-color dark:text-seventh-color pt-7 pb-2">
         Recorré el mundo <FontAwesomeIcon icon={faPlaneDeparture} />
       </h5>
       <h3 className="text-center text-xl font-bold text-first-color dark:text-second-color mb-4">
